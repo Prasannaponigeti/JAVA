ead3cf9e915596465365c6dc1a8491cd5a83ce72
Prasannaponigeti <151363628+Prasannaponigeti@users.noreply.github.com>
2026-02-19 16:16:14 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,29 @@
+public class Datatypes
+{
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println(num);
+
+        byte by = 127;
+        System.out.println(by);
+
+        short sh = 558;
+        System.out.println(sh);
+
+        long l = 53235;
+        System.out.println(l);
+
+        float f = 6.8f;
+        System.out.println(f);
+
+        double d = 6.8;
+        System.out.println(d);
+
+        char c = 'p';
+        System.out.println(c);
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
